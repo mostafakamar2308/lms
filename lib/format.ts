8794410762adf1bf0491049fd1d22ceac52f2ed7
mostafa <mostafakamar.dev@gmail.com>
2024-03-01 18:31:35 +0200

    fixed formating

--- lib/format.ts
+++ lib/format.ts
@@ -6,18 +6,18 @@ export const formatPrice = (price: number) => {
 };
 
 export function convertHMS(value: number) {
-  const sec = parseInt(value, 10); // convert value to number if it's string
-  let hours = Math.floor(sec / 3600); // get hours
-  let minutes = Math.floor((sec - hours * 3600) / 60); // get minutes
-  let seconds = sec - hours * 3600 - minutes * 60; //  get seconds
+  const sec = parseInt(String(value), 10); // convert value to number if it's string
+  let hours = String(Math.floor(sec / 3600)); // get hours
+  let minutes = String(Math.floor((sec - +hours * 3600) / 60)); // get minutes
+  let seconds = String(sec - +hours * 3600 - +minutes * 60); //  get seconds
   // add 0 if value < 10; Example: 2 => 02
-  if (hours < 10) {
+  if (+hours < 10) {
     hours = "0" + hours;
   }
-  if (minutes < 10) {
+  if (+minutes < 10) {
     minutes = "0" + minutes;
   }
-  if (seconds < 10) {
+  if (+seconds < 10) {
     seconds = "0" + seconds;
   }
 
